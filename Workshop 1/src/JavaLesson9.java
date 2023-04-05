f1ded66ab541387a06e3050d8906cc06af327c94
ahmet.pehlivan <ahmet.pehlivan@etiya.com>
2023-04-05 16:33:35 +0300

    Workshop 1

---
+++ Workshop 1/src/JavaLesson9.java
@@ -0,0 +1,15 @@
+public class JavaLesson9 {
+    public static void main(String[] args) {
+        int number1 = 30;
+        int number2 = 20;
+        int number3 = 10;
+
+        if (number1>=number2 && number1>=number3) {
+            System.out.println("En buyuk sayi : "+ number1);
+        } else if(number2>number1 && number2>number3) {
+            System.out.println("En buyuk sayi : "+ number2);
+        }else {
+            System.out.println("En buyuk sayi : "+ number3);
+        }
+    }
+}
